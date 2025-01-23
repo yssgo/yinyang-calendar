7a807ca7b3afc2bd3819c0a2ad4aed262644c16e
yssgo <cieltero@gmail.com>
2025-01-23 12:52:50 +0900

    Add files via upload

---
+++ holidays.mjs
@@ -0,0 +1,471 @@
+import {
+  //MonthTable,
+  //LunarTable,
+  LunarDate,
+  //nDaysYear,
+  nDaysMonth,
+  YunMonth,
+  //totalDays,
+  SolarToLunar
+} from "./lunar.mjs";
+export {
+  getBuddhaDay,
+  hasLunarDate,
+  getSeolChu,
+  getHolidaysSC,
+  extractHolidayListInMonth,
+  makeHolidaysText,
+  getHolidays
+};
+
+// Requirements:
+//
+//      lunar.js
+'use strict';
+
+// 음력 4월 8일 석가탄신일
+function getBuddhaDay(nowYear, nowMonth)
+{
+  let monthTable = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+  let monthIndex = nowMonth - 1;
+  let solar_year = nowYear;
+  let solar_month = monthIndex;
+  let solar_day = 1;
+
+  // 음력 표기 범위를 벗어난 년도에 관한 예외처리. 양력 데이터만 출력한다.
+  if ((solar_year < 1881) || (solar_year > 2051)
+     || ((solar_year == 2051) && (solar_month > 1)))
+  {
+    return -1;
+  }
+
+  if ((solar_year == 1881) && (solar_month == 0))
+  {
+    return -1;
+  }
+
+  if ((solar_year == 2051) && (solar_month == 1))
+  {
+    return -1;
+  }
+
+  let prevYear = solar_year - 1;
+  if (((prevYear % 4 == 0) && (prevYear % 100 != 0)) || (prevYear % 400 == 0))
+    monthTable[1] = 29;
+  let lunar_date = new LunarDate();
+  let solar_date = new Date(solar_year, solar_month, 1);
+  lunar_date = SolarToLunar(solar_date);
+
+  do
+  {
+    if (lunar_date.isYunMonth)
+    { ; //pass
+    }
+    else
+    {
+      if (lunar_date.month == 3 && lunar_date.day == 8)
+      {
+        return solar_day;
+      }
+    }
+    if (lunar_date.day >= nDaysMonth(lunar_date))
+    {
+      if (lunar_date.month < 11)
+      {
+        if ((lunar_date.month == YunMonth(lunar_date.year))
+           && !lunar_date.isYunMonth)
+        {
+          lunar_date.isYunMonth = true;
+          lunar_date.day = 1;
+        }
+        else
+        {
+          lunar_date.month++;
+          lunar_date.isYunMonth = false;
+          lunar_date.day = 1;
+        }
+      }
+      else
+      {
+        lunar_date.year++;
+        lunar_date.month = 0;
+        lunar_date.day = 1;
+      }
+    }
+    else
+      lunar_date.day++;
+    if (++solar_day > monthTable[solar_month])
+    {
+      break;
+    }
+  }
+  while (true);
+  return -1;
+}
+
+function hasLunarDate(solar_date)
+{
+  if (solar_date == null)
+  {
+    return false;
+  }
+  let Y = solar_date.getFullYear();
+  let M = solar_date.getMonth() + 1;
+  if ((Y < 1881) || (Y > 2051) || ((Y == 2051) && (M > 2)))
+  {
+    return false;
+  }
+  return true;
+}
+function getSeolChu(nowYear)
+{
+
+  let Year = nowYear;
+  let SeolDate = null;
+  let ChuDate = null;
+  let isSeolFound = false;
+  let isChuFound = false;
+  for (let Month = 1; Month <= 12 && (!isSeolFound || !isChuFound); Month++)
+  {
+    let FirstDay = 1;
+    let LastDay;
+    let SolarDate;
+    SolarDate = new Date(Year, Month - 1, FirstDay);
+    let EndDate = new Date(Year, Month, 0);
+    LastDay = EndDate.getDate();
+    if (!hasLunarDate(SolarDate))
+      break;
+    let lunar_date = SolarToLunar(SolarDate);
+    if (lunar_date == null)
+      break;
+    for (let Day = 1; Day <= LastDay && (!isSeolFound || !isChuFound); Day++)
+    {
+      if (lunar_date.month + 1 == 1 && lunar_date.day == 1)
+      {
+        SeolDate = new Date(Year, Month - 1, Day);
+        isSeolFound = true;
+        if (isChuFound)
+        {
+          break;
+        }
+      }
+      if (lunar_date.month + 1 == 8 && lunar_date.day == 15)
+      {
+        ChuDate = new Date(Year, Month - 1, Day);
+        isChuFound = true;
+        if (isSeolFound)
+        {
+          break;
+        }
+      }
+      if (lunar_date.day >= nDaysMonth(lunar_date))
+      {
+        if (lunar_date.month < 11)
+        {
+          if ((lunar_date.month == YunMonth(lunar_date.year)) && !lunar_date.isYunMonth)
+          {
+            lunar_date.isYunMonth = true;
+            lunar_date.day = 1;
+          }
+          else
+          {
+            lunar_date.month++;
+            lunar_date.isYunMonth = false;
+            lunar_date.day = 1;
+          }
+        }
+        else
+        {
+          lunar_date.year++;
+          lunar_date.month = 0;
+          lunar_date.day = 1;
+        }
+      }
+      else
+        lunar_date.day++;
+    }
+  }
+  return new Array(SeolDate, ChuDate);
+}
+
+function getHolidaysSC(FullYear, Month, inSeolDate, inChuDate)
+{
+
+  function BeforeAfter(SeolDate, holis, desc)
+  {
+    if (SeolDate != null)
+    {
+      let hMon = SeolDate.getMonth() + 1;
+      let hDay = SeolDate.getDate();
+      let hDesc = desc;
+      holis.push([hMon, hDay, hDesc]);
+
+      let Before = new Date(SeolDate.valueOf() - 86400000);
+      let After = new Date(parseInt(SeolDate.valueOf()) + 86400000);
+
+      if (Before.getFullYear() == SeolDate.getFullYear())
+      {
+        let hMon = parseInt(Before.getMonth()) + 1;
+        let hDay = Before.getDate();
+        let hDesc = desc + ' 전날';
+        holis.push([hMon, hDay, hDesc]);
+      }
+      if (After.getFullYear() == SeolDate.getFullYear())
+      {
+        let hMon = parseInt(After.getMonth()) + 1;
+        let hDay = After.getDate();
+        let hDesc = desc + ' 다음날';
+        holis.push([hMon, hDay, hDesc]);
+      }
+    }
+  }
+
+  let allholis2;
+  {
+    let i,
+    j,
+    hj;
+    let holis = new Array();
+    let subs = new Array();
+    let defaultholis = new Array(
+        [1, 1, "신정"],
+        [3, 1, "3·1절"],
+        [5, 5, "어린이날"],
+        [6, 6, "현충일"],
+        [8, 15, "광복절"],
+        [10, 3, "개천절"],
+        [10, 9, "한글날"],
+        [12, 25, "성탄절"]);
+
+    for (let m = 1; m <= 12; m++)
+    {
+      let buddha = getBuddhaDay(FullYear, m);
+      if (buddha >= 1)
+      {
+        defaultholis.push([m, buddha, "석가탄신일"]);
+        break;
+      }
+    }
+
+    for (i = 0; i < defaultholis.length; i++)
+    {
+      holis.push(defaultholis[i]);
+    }
+
+    {
+      let SeolDate = inSeolDate;
+      if (SeolDate != null)
+      {
+        SeolDate = new Date(FullYear, SeolDate.getMonth(), SeolDate.getDate());
+        BeforeAfter(SeolDate, holis, '설날');
+      }
+      let ChuDate = inChuDate;
+      if (ChuDate != null)
+      {
+        ChuDate = new Date(FullYear, ChuDate.getMonth(), ChuDate.getDate());
+        BeforeAfter(ChuDate, holis, '추석');
+      }
+    }
+    holis.sort(function (a, b)
+    {
+      return a[0] != b[0] ? a[0] - b[0] : a[1] - b[1];
+    }
+    );
+    {
+      function isTarget(desc)
+      {
+        let days = ["어린이날", "설날 전날", "설날", "설날 다음날", "추석 전날", "추석", "추석 다음날"];
+        let dfound = false;
+        for (let di = 0; di < days.length && !dfound; di++)
+        {
+          if (days[di] == desc)
+          {
+            dfound = true;
+            break;
+          }
+        }
+        return dfound;
+      };
+      let pivotYear = FullYear;
+      for (let pivot = 0; pivot < holis.length; pivot++)
+      {
+        let pivotMonth = holis[pivot][0];
+        let pivotDay = holis[pivot][1];
+        let pivotDesc = holis[pivot][2];
+        let pivotDate = new Date(pivotYear, pivotMonth - 1, pivotDay);
+        if (isTarget(pivotDesc))
+        {
+          let overlapped = false;
+          {
+            let holiDate = new Date(FullYear, pivotMonth - 1, pivotDay);
+            if (holiDate.getDay() == 0)
+            { //Sunday
+              overlapped = true;
+            }
+            if (!overlapped)
+            {
+              if (pivotDesc == '어린이날')
+              {
+                if (holiDate.getDay() == 6)
+                { //Saturday
+                  overlapped = true;
+                }
+              }
+            }
+            if (!overlapped)
+            {
+              for (let hj = 0; hj < holis.length; hj++)
+              {
+                if (pivot == hj)
+                  continue;
+                let hlMonth = holis[hj][0]
+                  let hlDay = holis[hj][1];
+                let hlDesc = holis[hj][2];
+                if (hlMonth == pivotMonth && hlDay == pivotDay)
+                {
+                  overlapped = true;
+                  break;
+                }
+              }
+            }
+          }
+          if (overlapped)
+          {
+            let nextDate = new Date(pivotDate.valueOf() + 86400000);
+            do
+            {
+              if (nextDate.getFullYear() != pivotYear)
+              {
+                break;
+              }
+              if (nextDate.getDay() != 0 && nextDate.getDay() != 6)
+              { // norml weekdays
+                let isOtherHoli = false;
+                for (let hj = 0; hj < holis.length && !isOtherHoli; hj++)
+                {
+                  let hlMonth = holis[hj][0]
+                    let hlDay = holis[hj][1];
+                  if (pivotMonth == hlMonth && pivotDay == hlDay)
+                    continue;
+                  if (hlMonth == parseInt(nextDate.getMonth()) + 1 && hlDay == nextDate.getDate())
+                  {
+                    isOtherHoli = true;
+                    break;
+                  }
+                }
+                let hMon = parseInt(nextDate.getMonth()) + 1;
+                let hDay = nextDate.getDate();
+                let hDesc = '대체 공휴일';
+                if (!isOtherHoli)
+                {
+                  subs.push(new Array(hMon, hDay, hDesc));
+                  break;
+                }
+              }
+              nextDate = new Date(nextDate.valueOf() + 86400000);
+            }
+            while (true);
+          }
+        }
+      }
+    }
+
+    let allholis = new Array();
+    for (i = 0; i < holis.length; i++)
+    {
+      allholis.push(holis[i]);
+    }
+    for (i = 0; i < subs.length; i++)
+    {
+      allholis.push(subs[i]);
+    }
+    allholis2 = new Array();
+    for (i = 0; i < allholis.length; i++)
+    {
+      let found = false;
+      let ahMonth = allholis[i][0];
+      let ahDay = allholis[i][1];
+      let ahDesc = allholis[i][2];
+      for (let j = 0; j < allholis2.length; j++)
+      {
+        let allh2Month = allholis2[j][0];
+        let allh2Day = allholis2[j][1];
+        if (allh2Month == ahMonth && allh2Day == ahDay)
+        {
+          let dlist = ahDesc.split(',');
+          let dupdesc = false;
+          let descElem = '';
+          for (let di = 0; di < dlist.length && !dupdesc; di++)
+          {
+            for (let si = 0; si < dlist[di].length; si++)
+            {
+              if (dlist[di][si] != ' ')
+              {
+                descElem += dlist[di][si];
+              }
+            }
+            if (descElem == ahDesc)
+            {
+              dupdesc = true;
+            }
+          }
+          if (!dupdesc)
+            allholis2[j][2] += ',' + ahDesc;
+          found = true;
+        }
+      }
+      if (!found)
+      {
+        allholis2.push(allholis[i]);
+      }
+    }
+    allholis2.sort(function (a, b)
+    {
+      return a[0] - b[0] ? a[0] - b[0] : a[1] - b[1];
+    }
+    );
+  }
+  return allholis2;
+}
+function extractHolidayListInMonth(holidays, FullYear, Month)
+{
+  let monthHolis = new Array();
+  if (holidays.length)
+  {
+    let endDate = null;
+    let lastday;
+    endDate = new Date(FullYear, parseInt(Month), 0);
+    lastday = endDate.getDate();
+    let m = Month;
+    for (let d = 1; d <= lastday; d++)
+    {
+      for (let i = 0; i < holidays.length; i++)
+      {
+        if (holidays[i][0] == m && holidays[i][1] == d)
+        {
+          monthHolis.push(holidays[i]);
+          break;
+        }
+      }
+    }
+  }
+  return monthHolis;
+}
+
+function makeHolidaysText(holidays, FullYear, Month)
+{
+  let t = ''
+    for (let hi = 0; hi < holidays.length; hi++)
+    {
+      t += holidays[hi][0] + '월 ' + holidays[hi][1] + '일 : ' + holidays[hi][2] + '\n';
+    }
+    return t;
+}
+
+function getHolidays(FullYear, Month)
+{
+  let SeolChu = getSeolChu(FullYear);
+  let inSeolDate = SeolChu[0];
+  let inChuDate = SeolChu[1];
+  return getHolidaysSC(FullYear, Month, inSeolDate, inChuDate);
+}
---
+++ yinyangcal.mjs
@@ -0,0 +1,439 @@
+import {
+  MonthTable,
+  //LunarTable,
+  LunarDate,
+  //nDaysYear,
+  nDaysMonth,
+  YunMonth,
+  totalDays,
+  SolarToLunar
+} from "./lunar.mjs";
+import {
+  //getBuddhaDay,
+  //hasLunarDate,
+  //getSeolChu,
+  //getHolidaysSC,
+  extractHolidayListInMonth,
+  makeHolidaysText,
+  getHolidays
+} from "./holidays.mjs";
+
+'use strict';
+const DEBUG = false;
+// currentDate는 lunar.js에서 옮겨왔음
+// 현재 페이지에 Display될 날짜에 관한 변수
+let currentDate = new Date();
+
+function elId(id) {
+  return document.querySelector('#' + id);
+}
+function elVal(id)
+{
+  return elId(id).value;
+}
+function removeHtml(el) {
+  while (el.hasChildNodes()) {
+    el.removeChild(el.firstChild);
+  }
+}
+function changeText(el, t) {
+  removeHtml(el);
+  let tn = document.createTextNode(t);
+  el.appendChild(tn);
+}
+
+// 페이지의 테이블에 계산된 달력 데이터를 삽입한다. (달력을 그린다.)
+class caldraw_class {
+  addBr (el) {
+    el.appendChild(document.createElement('br'));
+  }  
+  isToday (today, solar_date, solar_day) {
+    let date1 = new Date(today.valueOf());
+    date1.setHours(0, 0, 0, 0);
+    let date2 = new Date(solar_date.valueOf());
+    date2.setDate(solar_day);
+    date2.setHours(0, 0, 0, 0);
+    return date1.valueOf() == date2.valueOf();      
+  }    
+  search_holiday(holidays, solar_month, solar_day) {
+    let index = -1;
+    for (let hi = 0; hi < holidays.length && index == -1; hi++) {
+      if (holidays[hi][0] == solar_month + 1 
+          && holidays[hi][1] == solar_day) {
+        index = hi;
+      }
+    }
+    return index;
+  }
+  clean_lunar_cells(){
+    for (i = 0; i < 37; i++) {
+        let lundiv_id = "l" + i;
+        changeText(elId(lundiv_id), "");
+        elId(lundiv_id).className = "LunarBlank";
+      }
+  }
+  clean_lunar_cells_1881_0(){
+    for (let i = 0; i < 35; i++) {
+        let lundiv_id = "l" + i;
+        changeText(elId(lundiv_id), "");
+      }
+      changeText(elId("l35"), "1/1");
+      elId("l35").className = "LunarExtra";
+      changeText(elId("l36"), "1/2");
+      elId("l36").className = "LunarExtra";
+  }
+  clean_lunar_cells_2051_1(){
+    for (let i = 3; i < 13; i++) {
+        let lundiv_id = "l" + i;
+        changeText(elId(lundiv_id), "12/" + (i + 17));
+        elId(lundiv_id).className = "LunarExtra";
+        this.addBr(elId(lundiv_id));
+      }
+      for (let i = 13; i < 37; i++) {
+        let lundiv_id = "l" + i;
+        document.getElementById(lundiv_id).innerHTML = "";
+        changeText(elId(lundiv_id), "");
+        elId(lundiv_id).className = "LunarBlank";
+        this.addBr(elId(lundiv_id));
+      }
+  }
+  drawCalendar() {
+    let week;
+    let solar_date = new Date(elVal("txtYear"), elVal("txtMonth") - 1, 1);
+    let today = new Date();
+    today.setHours(0, 0, 0, 0);
+    changeText(elId("curYear"), solar_date.getFullYear());
+    changeText(elId("curMonth"), solar_date.getMonth() + 1);
+    
+    week = (totalDays(solar_date) + 1) % 7; // 현재 월의 첫번째 날짜의 요일을 계산 (0:월, 1:화, 2:수...)
+    
+    // 달력의 첫번째 날짜까지의 빈공란을 그린다.
+    for (let i = 0; i < week; i++) {
+      changeText(elId("s" + i), "-");
+      elId("s" + i).title = "";
+      elId("s" + i).className = "SolarBlank";
+      changeText(elId("l" + i), "");
+    }
+    
+    // 양력 날짜들을 각 칸에 삽입한다. if 절이 있는 이유는 일요일, 토요일의 색깔을 틀리게 하기위해.
+    let solar_year = solar_date.getFullYear();
+    let solar_day = solar_date.getDate();
+    let solar_month = solar_date.getMonth();
+    
+    let holidays = getHolidays(solar_year, solar_month + 1);
+    
+    do {
+      let soldiv_num = (solar_day + week - 1);
+      let soldiv_id = "s" + soldiv_num;
+      elId(soldiv_id).title = "";
+      elId(soldiv_id).className = "SolarWeekday";
+      changeText(elId(soldiv_id), solar_day);
+      if (soldiv_num % 7 == 0) {
+        elId(soldiv_id).className = "SolarSunday";
+      } else if (soldiv_num % 7 == 6) {
+        elId(soldiv_id).className = "SolarSaturday";
+      } else {
+        ; // pass
+      }
+      
+      if (this.isToday(today, solar_date, solar_day) == true) {
+        elId(soldiv_id).classList.add("SolarToday");
+      }
+      
+      this.addBr(elId(soldiv_id));
+      
+      let hindex;
+      hindex = this.search_holiday(holidays, solar_month, solar_day);
+      if (hindex != -1) {
+        elId(soldiv_id).classList.add("Holiday");
+        elId(soldiv_id).title = holidays[hindex][2];
+      }
+      
+    } while (++solar_day <= MonthTable[solar_month]);
+    
+    // 달력 마지막 날짜 이후의 빈공란을 그린다.
+    for (let i = week + MonthTable[solar_month]; i < 37; i++) {
+      let soldiv_id = "s" + i;
+      let lundiv_id = "l" + i;
+      changeText(elId(soldiv_id), "-");
+      elId(soldiv_id).title = "";
+      elId(soldiv_id).className = "SolarBlank";
+      changeText(elId(lundiv_id), "");
+      elId(lundiv_id).className = "LunarBlank";
+    }
+    
+    solar_date.setDate(1);
+    
+    // 음력 표기 범위를 벗어난 년도에 관한 예외처리. 양력 데이터만 출력한다.
+    
+    solar_month = solar_date.getMonth();
+    if ((solar_year < 1881) || (solar_year > 2051)
+        || ((solar_year == 2051) && (solar_month > 1))) {
+      clean_lunar_cells();      
+      return;
+    }
+    
+    if ((solar_year == 1881) && (solar_month == 0)) {
+      clean_lunar_cells_1881_0();      
+      return;
+    }
+    
+    if ((solar_year == 2051) && (solar_month == 1)) {
+      this.clean_lunar_cells_2051_1();
+      return;
+    }
+    
+    // 첫날짜를 음력으로 변경시켜 그 이후 날짜들을 증가시켜 음력날짜를 출력한다.
+    // 첫날짜만을 음력으로 변경하는 이유는 SolarToLunar()함수가 루프를 동반한 함수로써
+    // 상당히 느리기 때문에, 각 날짜마다 음력으로 변경시킨다는 건 낭비이기 때문이다.
+    // 따라서 코드는 좀 복잡해졌지만, 훨씬 빠르다.
+    let lunar_date = new LunarDate();
+    lunar_date = SolarToLunar(solar_date);
+    
+    solar_day = solar_date.getDate();
+    do {
+      
+      let lunar_prefix = "";
+      if (lunar_date.isYunMonth) {
+        lunar_prefix = "윤";
+      }
+      let lundiv_id = "l" + (solar_day + week - 1);
+      changeText(elId(lundiv_id),
+                  lunar_prefix + (lunar_date.month + 1) + "/" + lunar_date.day);
+      this.addBr(elId(lundiv_id));
+      elId(lundiv_id).className = "LunarNormal";
+      
+      if (lunar_date.day >= nDaysMonth(lunar_date)) {
+        if (lunar_date.month < 11) {
+          if ((lunar_date.month == YunMonth(lunar_date.year))
+                && !lunar_date.isYunMonth) {
+            lunar_date.isYunMonth = true;
+            lunar_date.day = 1;
+          } else {
+            lunar_date.month++;
+            lunar_date.isYunMonth = false;
+            lunar_date.day = 1;
+          }
+        } else {
+          lunar_date.year++;
+          lunar_date.month = 0;
+          lunar_date.day = 1;
+        }
+      } else
+        lunar_date.day++;
+      if (++solar_day > MonthTable[solar_month]) {
+        break;
+      }
+    } while (true);
+    ListHolidaysInMonth();
+    if (DEBUG == true) {
+      let o = document.getElementById("debug");
+      let yun = lunar_date.isYunMonth ? " 윤" : " ";
+      o.innerHTML = `<p>${lunar_date.year}.${yun}${lunar_date.month + 1}/${lunar_date.day}</p>`;
+    }
+  }
+
+}
+function drawCalendar(){
+  let caldraw = new caldraw_class();
+  caldraw.drawCalendar();
+}
+
+
+function btOK_onclick() {
+  let i,
+  j,
+  code,
+  hit;
+  
+  code = "0123456789";
+  
+  let str = elVal("txtYear");
+  
+  for (i = 1; i < str.length; i++) {
+    hit = 0;
+    for (j = 0; j < code.length; j++) {
+      if (str.charAt(i) == code.charAt(j)) {
+        hit = 1;
+        break;
+      }
+    }
+    if (!hit) {
+      alert("연도는 4자리 이하 숫자여야 합니다.");
+      elId("txtYear").value = "";
+      elId("txtYear").focus();
+      return;
+    }
+  }
+  
+  if (str.length > 4) {
+    alert("연도는 9999년도 이상 입력될 수 없습니다.");
+    elId("txtYear").value = "";
+    elId("txtYear").focus();
+    return;
+  }
+  
+  currentDate.setFullYear(elVal("txtYear"));
+  currentDate.setMonth(elVal("txtMonth") - 1);
+  
+  drawCalendar(currentDate);
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// 양/음력 만년달력 페이지의 이벤트 핸들링 함수들입니다.
+// by Albeniz
+
+function addDayCells() {
+  let ctable = document.querySelector("#calTable");
+  let cell;{
+    let r = 0;
+    for (cell = 0; r <= 5; r++) {
+      let row;
+      if (r == 5) {
+        row = ctable.rows[ctable.rows.length - 1];
+      } else {
+        row = ctable.insertRow(2 + r);
+      }
+      let lastcol = 6;
+      if (r == 5)
+        lastcol = 1;
+      for (let c = 0; c <= lastcol; c++, cell++) {
+        let col = row.insertCell(c);
+        let soldiv = document.createElement("div");
+        let lundiv = document.createElement("div");
+        col.className = "daycell";
+        soldiv.id = "s" + cell;
+        lundiv.id = "l" + cell;
+        col.appendChild(soldiv);
+        col.appendChild(lundiv);
+      }
+    }
+  }
+}
+function FillTodayDiv(){
+  let today = new Date();
+  today.setHours(0, 0, 0, 0);
+  let solar_date = new Date(today.valueOf());
+  let lunar_date = SolarToLunar(solar_date);
+  lunar_date.isYunMonth
+  let lunar_prefix = lunar_date
+.isYunMonth ? "윤": "";
+  changeText(elId('divTodaySol'),
+  solar_date.getFullYear() + "년 " +
+     (solar_date.getMonth() + 1) + "월 " +
+     solar_date.getDate() + "일");
+  changeText(elId('divTodayLun'),
+     "음력 " + lunar_prefix + 
+     (lunar_date.month + 1) + "월 " +
+     lunar_date.day + "일");
+}
+
+// 각 양식필드들에 관한 이벤트 핸들러 함수들
+function window_onload() {
+  FillTodayDiv();
+  addDayCells();
+  let today = new Date();
+  let dcells = document.querySelectorAll(".daycell");
+  let iterable = dcells;
+  for (let ci = 0; ci < dcells.length; ci++) {
+    dcells[ci].height = "50";
+    dcells[ci].width = "70";
+  }
+  elId("txtYear").value = today.getFullYear();
+  elId("txtMonth").value = today.getMonth() + 1;
+  
+  today.setFullYear(elVal("txtYear"));
+  today.setMonth(elVal("txtMonth") - 1);
+  currentDate = new Date(today);
+  drawCalendar(currentDate);
+}
+
+function btNextMonth_onclick() {
+  if (currentDate.getMonth() < 11) {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    elId("txtMonth").value = currentDate.getMonth() + 1;
+  } else {
+    currentDate.setFullYear(currentDate.getFullYear() + 1);
+    elId("txtYear").value = currentDate.getFullYear();
+    currentDate.setMonth(0);
+    elId("txtMonth").value = currentDate.getMonth() + 1;
+  }
+  
+  drawCalendar(currentDate);
+}
+
+
+function btNextYear_onclick() {
+  currentDate.setFullYear(currentDate.getFullYear() + 1);
+  elId("txtYear").value = currentDate.getFullYear();
+  drawCalendar(currentDate);
+}
+
+function btPrevMonth_onclick() {
+  if (currentDate.getMonth() > 0) {
+    currentDate.setMonth(currentDate.getMonth() - 1);
+    elId("txtMonth").value = currentDate.getMonth() + 1;
+  } else {
+    currentDate.setFullYear(currentDate.getFullYear() - 1);
+    elId("txtYear").value = currentDate.getFullYear();
+    currentDate.setMonth(11);
+    elId("txtMonth").value = currentDate.getMonth() + 1;
+  }
+  drawCalendar(currentDate);
+}
+
+function btPrevYear_onclick() {
+  
+  currentDate.setFullYear(currentDate.getFullYear() - 1);
+  elId("txtYear").value = currentDate.getFullYear();
+  
+  drawCalendar(currentDate);
+}
+
+function ListHolidaysInMonth() {
+  function getEl(idname) {
+    return document.getElementById(idname);
+  }
+  function removehtml(el) {
+    while (el.hasChildNodes()) {
+      el.removeChild(el.firstChild);
+    }
+  }
+  function changeText(idname, text) {
+    let el = getEl(idname);
+    removehtml(el);
+    el.appendChild(document.createTextNode(text));
+  }
+  let FullYear = currentDate.getFullYear();
+  let Month = currentDate.getMonth() + 1;
+  let holidays = getHolidays(FullYear);
+  let holidaysInMonth = extractHolidayListInMonth(holidays, FullYear, Month);
+  let MonthHolidaysText = makeHolidaysText(holidaysInMonth, FullYear, Month);
+  changeText('holiList', MonthHolidaysText);
+}
+
+window.addEventListener (
+  "load",
+  function () {
+    elId("btNextMonth")
+      .addEventListener(
+        "click",
+        btNextMonth_onclick);
+    elId("btOK")
+      .addEventListener(
+        "click",
+        btOK_onclick);
+    elId("btNextYear")
+      .addEventListener(
+        "click",
+        btNextYear_onclick);
+    elId("btPrevMonth")
+      .addEventListener(
+        "click",
+        btPrevMonth_onclick);
+    elId("btPrevYear")
+      .addEventListener(
+        "click",
+        btPrevYear_onclick);
+    window_onload();
+  }
+)
